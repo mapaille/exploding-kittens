666ef64706a6d5e110951c6be2f64df112f42e25
mapaille <mpaille@live.ca>
2024-12-08 09:02:43 -0500

    Add attack image.

--- src/Mapaille.ExplodingKittens.WebApp/Components/Card.razor.cs
+++ src/Mapaille.ExplodingKittens.WebApp/Components/Card.razor.cs
@@ -18,6 +18,7 @@ public partial class Card
         return cardType switch
         {
             CardType.ExplodingKitten => "exploding-kitten",
+            CardType.Attack => $"attack",
             CardType.Cat1 => $"cat-1",
             CardType.Cat2 => $"cat-2",
             CardType.Cat3 => $"cat-3",
