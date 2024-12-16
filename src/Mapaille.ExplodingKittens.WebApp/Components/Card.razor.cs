d771325f222d14b7c73defbfe6e3171e32bf56f9
mapaille <mpaille@live.ca>
2024-12-16 09:58:43 -0500

    Add divination, favor and skip card images.

--- src/Mapaille.ExplodingKittens.WebApp/Components/Card.razor.cs
+++ src/Mapaille.ExplodingKittens.WebApp/Components/Card.razor.cs
@@ -24,9 +24,12 @@ public partial class Card
             CardType.Cat3 => "cat-3",
             CardType.Cat4 => "cat-4",
             CardType.Cat5 => "cat-5",
+            CardType.Divination => "divination",
             CardType.Defuse => "defuse",
+            CardType.Favor => "favor",
             CardType.Nope => "nope",
             CardType.Shuffle => "shuffle",
+            CardType.Skip => "skip",
             _ => string.Empty,
         };
     }
