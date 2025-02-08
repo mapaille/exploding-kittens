127ea573d7807a275a0402351a2aea3559d564ed
mapaille <mpaille@live.ca>
2025-02-08 18:50:05 -0500

    Add snowflakes

--- src/Mapaille.ExplodingKittens.WebApp/wwwroot/scripts.js
+++ src/Mapaille.ExplodingKittens.WebApp/wwwroot/scripts.js
@@ -2,37 +2,53 @@
     alert(message);
 }
 
-const createFallingHearts = () => {
+const createFallingEffects = () => {
 
     // Create the container
-    const loveContainer = document.createElement('div');
-    loveContainer.id = 'love-container';
+    const container = document.createElement('div');
 
-    // Style it (optional, could be done in CSS/SCSS)
-    loveContainer.style.position = 'fixed'; // or 'absolute' based on your layout
-    loveContainer.style.top = '0';
-    loveContainer.style.left = '0';
-    loveContainer.style.width = '100%';
-    loveContainer.style.height = '100vh'; // full viewport height
-    loveContainer.style.pointerEvents = 'none'; // so it doesn't interfere with clicks
+    container.id = 'falling-container';
+    container.style.position = 'fixed'; // or 'absolute' based on your layout
+    container.style.top = '0';
+    container.style.left = '0';
+    container.style.width = '100%';
+    container.style.height = '100vh'; // full viewport height
+    container.style.pointerEvents = 'none'; // so it doesn't interfere with clicks
 
     // Append it to the body or another element
-    document.body.appendChild(loveContainer);
+    document.body.appendChild(container);
 
     setInterval(() => {
         const heart = document.createElement('div');
-        heart.textContent = '❤️';
-        heart.style.fontSize = '1.5rem';
+
+        heart.textContent = '❤';
         heart.classList.add('falling-heart');
         heart.style.left = `${Math.random() * 100}%`;
         heart.style.animationDuration = `${Math.random() * 5 + 5}s`; // Random duration for variety
-        loveContainer.appendChild(heart);
+
+        container.appendChild(heart);
 
         // Remove heart after animation
         setTimeout(() => {
             heart.remove();
         }, 15000);
     }, 1000); // Adjust interval to control heart frequency
+
+    setInterval(() => {
+        const snowflake = document.createElement('div');
+
+        snowflake.textContent = '❄';
+        snowflake.classList.add('falling-snowflake');
+        snowflake.style.left = `${Math.random() * 100}%`;
+        snowflake.style.animationDuration = `${Math.random() * 5 + 5}s`; // Random duration for variety
+
+        container.appendChild(snowflake);
+
+        // Remove heart after animation
+        setTimeout(() => {
+            snowflake.remove();
+        }, 15000);
+    }, 1000); // Adjust interval to control heart frequency
 }
 
-window.onload = createFallingHearts;
+window.onload = createFallingEffects;
